19de1ae2be99149b89d6d9c7706f1d5ba74b8239
ethan <ethan@LAPTOP-FHOLCKVQ.lan>
2021-03-21 14:28:04 -0700

    21/03 (2)

--- src/extra/SwingReview.java
+++ src/extra/SwingReview.java
@@ -32,58 +32,85 @@ public class SwingReview implements ActionListener, KeyListener {
 
 	public void run() throws Exception {
 		// 1. Initialize your JFrame variable as an object of the JFrame class
-		
+		frame = new JFrame();
+
 		// 2. Set your JFrame object to be visible
-		// JFrames are hidden by default! Make sure you are telling it to set visible, not asking if it is visible!
+		// JFrames are hidden by default! Make sure you are telling it to set visible,
+		// not asking if it is visible!
+		frame.setVisible(true);
 
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
+		// yes
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-		// This makes the program actually close when we hit the red X in the corner! Without it, our program keeps
+		// This makes the program actually close when we hit the red X in the corner!
+		// Without it, our program keeps
 		// running in the background even after we hit the red X.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 5. Initialize your JPanel variable as an object of the JPanel class
+		panel = new JPanel();
 
 		// 6. Initialize your JLabel variable as an object of the JLabel class
+		label = new JLabel();
 
 		// 7. Set the text of the JLabel to a lovely greeting.
+		label.setText("You look fantastic today!");
 
 		// 8. Add the JPanel object to the JFrame
+		frame.add(panel);
 
 		// 9. Add the JLabel object to the JPanel
+		panel.add(label);
 
 		// 10. Pack your JFrame.
+		frame.pack();
 
 		// 11. Run your program again. Do you see your message?
+		// yes
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
+		loadImage("java.png");
 
 		// 13. Re-pack the JFrame object.
+		frame.pack();
 
 		// 14. Run your program again. Do you see the image?
+		// no
 
 		// 15. Initialize your first JButton variable as an object of the JButton class
+		button1 = new JButton();
 
 		// 16. Add the JButton object to the JPanel
+		panel.add(button1);
 
 		// 17. Pack your JFrame
+		frame.pack();
 
 		// 18. Add an action listener to the button
+		button1.addActionListener(this);
 
 		// 19. Set the text of your button to "Click Me"
+		button1.setText("Click me!");
 
 		// 20. Pack your JFrame
+		frame.pack();
 
 		// 21. Add a second JButton and action listener to the button
+		button2 = new JButton();
+		panel.add(button2);
+		button2.addActionListener(this);
 
 		// 22. Set the text of your button to "No, Click Me"
+		button2.setText("No, click me!");
 
 		// 23. Uncomment the 2 lines below (Ignore what they do for now)
-		// button1.setFocusable(false);
-		// button2.setFocusable(false);
+		button1.setFocusable(false);
+		button2.setFocusable(false);
 
 		// 29. Add a key listener to the frame
+		frame.addKeyListener(this);
 
 	}
 
@@ -95,17 +122,25 @@ public class SwingReview implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// 24. Uncomment the 2 lines below
-		// JButton buttonClicked = (JButton)e.getSource();
-		// System.out.println(buttonClicked.getText());
+		JButton buttonClicked = (JButton)e.getSource();
+		System.out.println(buttonClicked.getText());
 
 		// 25. Check which button was clicked
 		// 26. If the "Click Me" button was clicked, use JOptionPane pop-up to say "Good
 		// Choice"
+		if (button1.isEnabled()) {
+			JOptionPane.showMessageDialog(null, "Good choice!");
+		}
 
 		// 27. If the "No, Click Me button" was clicked, say "A Better Choice"
-
-		// 28. If the "Click Me" button is clicked 3 times, change the text of the button
+		else if (button2.isEnabled()) {
+			JOptionPane.showMessageDialog(null, "A better choice!");
+		}
+		
+		// 28. If the "Click Me" button is clicked 3 times, change the text of the
+		// button
 		// to say "Stop Clicking Me"
+		
 
 	}
 
